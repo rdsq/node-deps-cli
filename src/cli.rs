568198bfaa8dffb3d7a6f255332f9c377c9373f5
rdsq <62446963+rdsq@users.noreply.github.com>
2025-03-29 20:09:02 +0200

    and help message

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,6 @@
-const HELP_MESSAGE: &str = r#"Help wip"#;
+const HELP_MESSAGE: &str = r#"Show Node.js package's dependencies in a fancy way
+
+Usage: node-deps [path to package (current dir by default)]"#;
 
 pub fn get_path_input() -> String {
     if let Some(input_arg) = std::env::args().nth(1) {
