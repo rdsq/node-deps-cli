b028cc1acc446b5fbc3687073628cdb157259670
rdsq <62446963+rdsq@users.noreply.github.com>
2025-03-29 22:00:01 +0200

    more of help string

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,14 @@
 const HELP_MESSAGE: &str = r#"Show Node.js package's dependencies in a fancy way
 
-Usage: node-deps [path to package (current dir by default)]"#;
+Usage: node-deps
+
+Simply run this command inside a Node package, and you will see its dependencies
+
+You can also add path like this:
+
+node deps ./path/to/package
+
+And see that directory's dependencies. It looks up the current directory by default"#;
 
 pub fn get_path_input() -> String {
     if let Some(input_arg) = std::env::args().nth(1) {
