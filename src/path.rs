fff3f6a2fc50f7b7cd24aa8ae3318b35a5b1729c
rdsq <62446963+rdsq@users.noreply.github.com>
2025-04-26 18:33:34 +0000

    handle dir and file inputs better

--- src/path.rs
+++ src/path.rs
@@ -1,12 +1,11 @@
 use std::path::PathBuf;
-use std::ffi::OsStr;
 
 const FILE_NAME: &str = "package.json";
 
 pub fn create_path(input: &str) -> String {
     let mut path = PathBuf::from(input);
-    if path.file_name() != Some(OsStr::new(FILE_NAME)) {
-        // if not includes then add this part
+    if path.is_dir() {
+        // add file if this is a directory
         path.push(FILE_NAME);
     }
     path.to_str()
